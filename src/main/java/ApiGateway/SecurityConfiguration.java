b4ce94f186015d49dbbca3697b9c467ee95dfdbe
Luis René Panjón <luchorene97@gmail.com>
2022-12-15 12:27:19 -0500

    Añadir configuraciones de conexión a servidor de autenticación.

--- src/main/java/ApiGateway/SecurityConfiguration.java
+++ src/main/java/ApiGateway/SecurityConfiguration.java
@@ -3,7 +3,6 @@ package ApiGateway;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -15,10 +14,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
         httpSecurity
-                .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
-                .oauth2Login(Customizer.withDefaults());
-        httpSecurity.csrf().disable();
-
+                .httpBasic().disable().csrf().disable();
         return httpSecurity.build();
     }
 
